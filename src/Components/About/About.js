bc82aee77bb9e0ad5c16d50c17cdebbb6bb7bd7f
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-10-04 21:18:49 +0600

    Contact Added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,14 +4,22 @@ import AboutImage from '../../Images/about.jpg';
 const About = () => {
     return (
         <div className="About container">
-            <h1 className="section-title">About Us</h1>
+            <h2 className="section-title">About Us</h2>
             <div className="d-flex justify-content-center">
                 <img className="about-image" src={AboutImage} alt="" />
             </div>
-            <h4>What is Programming Zone?</h4>
+            <h4 className="mt-5">What is Programming Zone?</h4>
             <p>We create simplified and interactive learning experiences.
                 Learning programming should be easy to understand and available for everyone, everywhere!
                 Programming Zone is a school for programmers, covering all the aspects of software development.</p>
+            <p>Programming Zone originated from the idea that there exists a class of readers who respond better to
+                online content and prefer to learn new skills at their own pace from the comforts of their drawing rooms.
+                The journey commenced with a single tutorial on HTML in 2021 and elated by the response it generated, we worked our
+                way to adding fresh tutorials to our repository which now proudly flaunts a wealth of tutorials and allied articles
+                on topics ranging from programming languages to web designing to academics and much more.</p>
+            <p>Our content and resources are freely available and we prefer to keep it that way to encourage our readers acquire
+                as many skills as they would like to. We don’t force our readers to sign up with us or
+                submit their details either. No preconditions and no impediments. Simply Easy Learning!</p>
         </div>
     );
 };
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -3,8 +3,30 @@ import './Contact.css';
 
 const Contact = () => {
     return (
-        <div>
-            
+        <div className="Contact container">
+            <div className="row">
+                <h2 className="section-title mt-5">Contact Us</h2>
+                <div className="col-md-6 col-12">
+                    <h3 className="mt-3">Leave us a message</h3>
+                    <div className="mb-3">
+                        <label className="form-label">Full Name</label>
+                        <input type="text" className="form-control" placeholder="Your Name" />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Email address</label>
+                        <input type="email" className="form-control" placeholder="Your Email" />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Message</label>
+                        <textarea className="form-control" rows="4"></textarea>
+                    </div>
+                    <button className="btn px-4 send-button"><i className="fas fa-paper-plane"></i> Send</button>
+                </div>
+                <div className="col-md-6 col-12">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4444.1648048738925!2d-74.06152056849277!3d40.72269242714635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c250cc15ab9c41%3A0x938874ec5d431e78!2s542-556%20Montgomery%20St%2C%20Jersey%20City%2C%20NJ%2007302%2C%20USA!5e0!3m2!1sen!2sbd!4v1633359818329!5m2!1sen!2sbd" 
+                    className="google-map" title="google map" loading="lazy"></iframe>
+                </div>
+            </div>
         </div>
     );
 };
