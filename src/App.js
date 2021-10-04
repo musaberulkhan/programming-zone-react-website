1651d74dc7f22587ae29e69bb0d31580222995f4
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-10-04 20:02:45 +0600

    Course Details Added

--- src/App.js
+++ src/App.js
@@ -21,13 +21,13 @@ function App() {
               <img className="brand-logo" src={Logo} alt="" />
               <Link className="navbar-brand" to="/">Programming Zone</Link>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+                <span className="navbar-toggler-icon d-flex align-items-center"><i className="fas fa-bars"></i></span>
               </button>
               <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
                   <Link className="nav-link" to="/"><i className="fas fa-home"></i> Home</Link>
                   <Link className="nav-link" to="/courses"><i className="fas fa-list"></i> Courses</Link>
-                  <Link className="nav-link" to="/about"><i className="fas fa-list"></i> About</Link>
+                  <Link className="nav-link" to="/about"><i className="fas fa-info-circle"></i> About</Link>
                   <Link className="nav-link" to="/contact"><i className="fas fa-envelope-open-text"></i> Contact</Link>
                   <Link className="nav-link" to="/login"><i className="fas fa-user"></i> Login</Link>
                 </div>
--- src/Components/CourseDetails/CourseDetails.js
+++ src/Components/CourseDetails/CourseDetails.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './CourseDetails.css';
+import { useParams } from 'react-router-dom';
+import useCourses from '../../hooks/useCourses';
 
 const CourseDetails = () => {
+    const { courseId } = useParams();
+    const [course, setCourse] = useState();
+
+    useEffect(() => {
+        fetch('../coursedata.json')
+            .then(res => res.json())
+            .then(data => {
+                data.forEach(course => {
+                    if (course.id === courseId) {
+                        setCourse(course);
+                    }
+                });
+            })
+    }, [courseId]);
+
     return (
-        <div>
-            AAAA
+        <div className="CourseDetails container my-5">
+            <h1 className="text-center mb-4">{course?.course}</h1>
+            <div className="d-flex justify-content-center mb-5">
+                <img className="image" src={course?.image} alt="" />
+            </div>
+            <h6>Instructor: {course?.instructor}</h6>
+            <h5>Course Fee: ${course?.price}</h5>
+            <h6>Duration: {course?.duaration}</h6>
+            <p className="details">{course?.details}</p>
         </div>
     );
 };
