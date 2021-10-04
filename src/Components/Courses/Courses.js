9fea783d7bc8d767bdad507923e6c9de97c7c5d0
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-10-04 18:45:47 +0600

    Courses Added

--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import useCourses from '../../hooks/useCourses';
+import Course from '../Course/Course';
 import './Courses.css';
 
 const Courses = () => {
+    const [courses] = useCourses();
+
     return (
-        <div className="Courses">
-            
+        <div className="Courses container">
+            <h2 className="mt-5 section-title">All Courses</h2>
+            <div className="display-courses">
+                {
+                    courses.map(course => <Course key={course.id} course={course}></Course>)
+                }
+            </div>            
         </div>
+
     );
 };
 
